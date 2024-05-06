96a35d39c0e5e382e1264df78ef66b8e945724aa
Dominik Chrastecky <dominik@chrastecky.cz>
2024-05-06 23:18:52 +0200

    Store image in background

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/helper/AlarmManagerHelper.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/helper/AlarmManagerHelper.java
@@ -1,12 +1,10 @@
 package cz.chrastecky.aiwallpaperchanger.helper;
 
-import static android.content.Context.ALARM_SERVICE;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.util.Log;
 
 import java.util.Calendar;
 
@@ -26,39 +24,39 @@ public class AlarmManagerHelper {
     }
 
     public static void scheduleAlarm(Context context) {
-        SharedPreferences sharedPreferences = new SharedPreferencesHelper().get(context);
-        if (!sharedPreferences.contains("selectedInterval")) {
-            throw new RuntimeException("Selected interval must be populated when scheduling alarm");
-        }
-        int interval = sharedPreferences.getInt("selectedInterval", 0);
-
-        PendingIntent intent = getAlarmIntent(context);
-
-        Calendar target = Calendar.getInstance();
-        double howOften = 24.0 / interval;
-        target.set(Calendar.HOUR_OF_DAY, 0);
-        target.set(Calendar.MINUTE, 0);
-        target.set(Calendar.SECOND, 0);
-        int howOftenMilliseconds = (int) (howOften * 60 * 60 * 1_000);
-
-        Calendar now = Calendar.getInstance();
-        while (!target.after(now)) {
-            target.setTimeInMillis(target.getTimeInMillis() + howOftenMilliseconds);
-        }
-
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        alarmManager.cancel(AlarmManagerHelper.getAlarmIntent(context, PendingIntent.FLAG_NO_CREATE | PendingIntent.FLAG_IMMUTABLE));
-        alarmManager.setRepeating(
-                AlarmManager.RTC,
-                target.getTimeInMillis(),
-                (long) ((24.0 / interval) * 60 * 60 * 1_000),
-                intent
-        );
-
-//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+//        SharedPreferences sharedPreferences = new SharedPreferencesHelper().get(context);
+//        if (!sharedPreferences.contains("selectedInterval")) {
+//            throw new RuntimeException("Selected interval must be populated when scheduling alarm");
+//        }
+//        int interval = sharedPreferences.getInt("selectedInterval", 0);
+//
+//        PendingIntent intent = getAlarmIntent(context);
+//
+//        Calendar target = Calendar.getInstance();
+//        double howOften = 24.0 / interval;
+//        target.set(Calendar.HOUR_OF_DAY, 0);
+//        target.set(Calendar.MINUTE, 0);
+//        target.set(Calendar.SECOND, 0);
+//        int howOftenMilliseconds = (int) (howOften * 60 * 60 * 1_000);
+//
 //        Calendar now = Calendar.getInstance();
-//        now.add(Calendar.MINUTE, 1);
-//        alarmManager.set(AlarmManager.RTC, now.getTimeInMillis(), getAlarmIntent(context));
-//        Log.d("Schedule", "Should run at " + now.getTime());
+//        while (!target.after(now)) {
+//            target.setTimeInMillis(target.getTimeInMillis() + howOftenMilliseconds);
+//        }
+//
+//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+//        alarmManager.cancel(AlarmManagerHelper.getAlarmIntent(context, PendingIntent.FLAG_NO_CREATE | PendingIntent.FLAG_IMMUTABLE));
+//        alarmManager.setRepeating(
+//                AlarmManager.RTC,
+//                target.getTimeInMillis(),
+//                (long) ((24.0 / interval) * 60 * 60 * 1_000),
+//                intent
+//        );
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE, 1);
+        alarmManager.set(AlarmManager.RTC, now.getTimeInMillis(), getAlarmIntent(context));
+        Log.d("Schedule", "Should run at " + now.getTime());
     }
 }
