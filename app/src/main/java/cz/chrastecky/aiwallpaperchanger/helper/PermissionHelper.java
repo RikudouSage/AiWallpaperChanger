dd7e89a18287ad756de9cbc97bd9fd5977484d5c
Rikudou_Sage <dominik@chrastecky.cz>
2024-05-26 14:37:40 +0200

    Feat: Ask for data saver exception (#27)

---
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/helper/PermissionHelper.java
@@ -0,0 +1,58 @@
+package cz.chrastecky.aiwallpaperchanger.helper;
+
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.provider.Settings;
+
+import cz.chrastecky.aiwallpaperchanger.R;
+
+// todo doze mode
+public class PermissionHelper {
+    public static boolean shouldCheckForPermissions(Context context) {
+        PendingIntent pendingIntent = AlarmManagerHelper.getAlarmIntent(context, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_NO_CREATE);
+        return pendingIntent != null;
+    }
+
+    public static void askForDataSaverException(Context context) {
+        final String doNotAskAgainKey = "data_saver_exception_do_not_ask";
+        SharedPreferences preferences = new SharedPreferencesHelper().get(context);
+
+//        SharedPreferences.Editor ed = preferences.edit();
+//        ed.remove(doNotAskAgainKey);
+//        ed.apply();
+
+        if (preferences.contains(doNotAskAgainKey)) {
+            return;
+        }
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        int status =  connectivityManager.getRestrictBackgroundStatus();
+
+        if (status != ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED) {
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.app_permission_data_saver_dialog_body);
+        builder.setTitle(R.string.app_permission_data_saver_dialog_title);
+        builder.setPositiveButton(R.string.app_permission_dialog_button_exception, (dialog, which) -> {
+            Intent intent = new Intent(Settings.ACTION_IGNORE_BACKGROUND_DATA_RESTRICTIONS_SETTINGS);
+            intent.setData(Uri.fromParts("package", context.getPackageName(), null));
+            context.startActivity(intent);
+        });
+        builder.setNeutralButton(R.string.app_permission_dialog_button_ignore, (dialog, which) -> {});
+        builder.setNegativeButton(R.string.app_permission_dialog_button_dont_ask_again, (dialog, which) -> {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean(doNotAskAgainKey, true);
+            editor.apply();
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
