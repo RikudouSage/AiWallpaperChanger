2263d7f05a35e5b1fd07f7afbefe60c0ad79f58b
Dominik Chrastecky <dominik@chrastecky.cz>
2024-06-15 19:41:20 +0200

    Sort styles by name

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/helper/PremadePromptHelper.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/helper/PremadePromptHelper.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 import cz.chrastecky.aiwallpaperchanger.R;
@@ -17,7 +19,11 @@ public class PremadePromptHelper {
     public static PremadePrompt[] getPrompts(Context context) throws IOException {
         try (InputStream source = context.getResources().openRawResource(R.raw.premade_prompts)) {
             String text = new BufferedReader(new InputStreamReader(source)).lines().collect(Collectors.joining("\n"));
-            return new Gson().fromJson(text, PremadePrompt[].class);
+            return Arrays.stream(
+                    new Gson().fromJson(text, PremadePrompt[].class)
+            )
+                    .sorted(Comparator.comparing(PremadePrompt::getName))
+                    .toArray(PremadePrompt[]::new);
         }
     }
 }
