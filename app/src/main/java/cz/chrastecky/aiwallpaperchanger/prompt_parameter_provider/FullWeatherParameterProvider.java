26e5fb53806e386dfdc192f4df604197210f9199
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-01 15:08:50 +0200

    Use a map for weather codes

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/prompt_parameter_provider/FullWeatherParameterProvider.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/prompt_parameter_provider/FullWeatherParameterProvider.java
@@ -8,7 +8,9 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import cz.chrastecky.aiwallpaperchanger.BuildConfig;
@@ -43,41 +45,68 @@ public class FullWeatherParameterProvider extends AbstractLocationParameterProvi
                     return;
                 }
 
+                final Map<Integer, String> weatherMap = new HashMap<Integer, String>() {{
+                    put(200, "thunderstorm with light rain");
+                    put(201, "thunderstorm with rain");
+                    put(202, "thunderstorm with heavy rain");
+                    put(210, "light thunderstorm");
+                    put(211, "thunderstorm");
+                    put(212, "heavy thunderstorm");
+                    put(221, "ragged thunderstorm");
+                    put(230, "thunderstorm with light drizzle");
+                    put(231, "thunderstorm with drizzle");
+                    put(232, "thunderstorm with heavy drizzle");
+                    put(300, "light intensity drizzle");
+                    put(301, "drizzle");
+                    put(302, "heavy intensity drizzle");
+                    put(310, "light intensity drizzle rain");
+                    put(311, "drizzle rain");
+                    put(312, "heavy intensity drizzle rain");
+                    put(313, "shower rain and drizzle");
+                    put(314, "heavy shower rain and drizzle");
+                    put(321, "shower drizzle");
+                    put(500, "light rain");
+                    put(501, "moderate rain");
+                    put(502, "heavy intensity rain");
+                    put(503, "very heavy rain");
+                    put(504, "extreme rain");
+                    put(511, "freezing rain");
+                    put(520, "light intensity shower rain");
+                    put(521, "shower rain");
+                    put(522, "heavy intensity shower rain");
+                    put(531, "ragged shower rain");
+                    put(600, "light snow");
+                    put(601, "snow");
+                    put(602, "heavy snow");
+                    put(611, "sleet");
+                    put(612, "light shower sleet");
+                    put(613, "shower sleet");
+                    put(615, "light rain and snow");
+                    put(616, "rain and snow");
+                    put(620, "light shower snow");
+                    put(621, "shower snow");
+                    put(622, "heavy shower snow");
+                    put(701, "mist");
+                    put(711, "smoke");
+                    put(721, "haze");
+                    put(731, "sand/dust whirls");
+                    put(741, "fog");
+                    put(751, "sand");
+                    put(761, "dust");
+                    put(762, "volcanic ash");
+                    put(771, "squalls");
+                    put(781, "tornado");
+                    put(800, "clear sky");
+                    put(801, "few clouds");
+                    put(802, "scattered clouds");
+                    put(803, "broken clouds");
+                    put(804, "overcast clouds");
+                }};
+
                 final int code = result.getWeather().get(0).getId();
-                switch (code) {
-                    case 200:
-                        future.complete("thunderstorm with light rain");
-                        break;
-                    case 201:
-                        future.complete("thunderstorm with rain");
-                        break;
-                    case 202:
-                        future.complete("thunderstorm with heavy rain");
-                        break;
-                    case 210:
-                        future.complete("light thunderstorm");
-                        break;
-                    case 211:
-                        future.complete("thunderstorm");
-                        break;
-                    case 212:
-                        future.complete("heavy thunderstorm");
-                        break;
-                    case 221:
-                        future.complete("ragged thunderstorm");
-                        break;
-                    case 230:
-                        future.complete("thunderstorm with light drizzle");
-                        break;
-                    case 231:
-                        future.complete("thunderstorm with drizzle");
-                        break;
-                    case 232:
-                        future.complete("thunderstorm with heavy drizzle");
-                        break;
-                    default:
-                        logger.debug("FullWeather", "Unknown weather code: " + code);
-                        future.complete("unknown");
+                future.complete(weatherMap.getOrDefault(code, "unknown"));
+                if (future.join().equals("unknown")) {
+                    logger.debug("FullWeather", "Unknown weather code: " + code);
                 }
 
                 setCache(future.join(), parameterName);
