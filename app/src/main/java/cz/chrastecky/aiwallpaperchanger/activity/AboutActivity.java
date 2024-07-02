0037bba5f7b8d744f7bb2480fc8c67fa5900116f
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-02 12:19:32 +0200

    Add logcat logging

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/AboutActivity.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/AboutActivity.java
@@ -2,6 +2,7 @@ package cz.chrastecky.aiwallpaperchanger.activity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.text.method.LinkMovementMethod;
@@ -11,11 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ShareCompat;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import cz.chrastecky.aiwallpaperchanger.BuildConfig;
 import cz.chrastecky.aiwallpaperchanger.R;
 import cz.chrastecky.aiwallpaperchanger.databinding.ActivityAboutBinding;
+import cz.chrastecky.aiwallpaperchanger.helper.Logger;
 import cz.chrastecky.aiwallpaperchanger.sharing.AppFileProvider;
 
 public class AboutActivity extends AppCompatActivity {
@@ -40,20 +47,62 @@ public class AboutActivity extends AppCompatActivity {
                 Toast.makeText(this, R.string.app_about_share_logs_no_log_file, Toast.LENGTH_LONG).show();
                 return;
             }
+            createLogCatFile();
+            File logcatFile = new File(getFilesDir() + "/logs", "logcat.log");
 
-            Uri imageUri = AppFileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".files_provider", logFile);
+            ArrayList<Uri> attachments = new ArrayList<>();
+            attachments.add(AppFileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".files_provider", logFile));
+            if (logcatFile.exists()) {
+                attachments.add(AppFileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".files_provider", logcatFile));
+            }
 
-            Intent intent = new ShareCompat.IntentBuilder(this)
-                    .setType("message/rfc822")
-                    .setEmailTo(new String[]{BuildConfig.SUPPORT_EMAIL})
-                    .setStream(imageUri)
-                    .setSubject("AI Wallpaper Changer logs")
-                    .setText("Hi there!\n\nI'm sending the logs for AI Wallpaper Changer attached.")
-                    .setChooserTitle(getString(R.string.app_about_share_logs_chooser_title))
-                    .createChooserIntent()
-                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+            intent.setType("message/rfc822");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "AI Wallpaper Changer logs");
+            intent.putExtra(Intent.EXTRA_TEXT, "Hi there!\n\nI'm sending the logs for AI Wallpaper Changer attached.");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{BuildConfig.SUPPORT_EMAIL});
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, attachments);
+            intent = Intent.createChooser(intent, getString(R.string.app_about_share_logs_chooser_title));
 
             startActivity(intent);
         });
+    }
+
+    private void createLogCatFile() {
+        final String cmd = "logcat -d -v time";
+        InputStreamReader reader = null;
+
+        File logcat = new File(getFilesDir() + "/logs/logcat.log");
+        if (logcat.exists()) {
+            logcat.delete();
+        }
+
+        try (FileWriter writer = new FileWriter(logcat)) {
+            Process process = Runtime.getRuntime().exec(cmd);
+            reader = new InputStreamReader(process.getInputStream());
+            writer.write ("App version: " + BuildConfig.VERSION_NAME + "\n");
+
+            char[] buffer = new char[10000];
+            do {
+                int charsRead = reader.read(buffer, 0, buffer.length);
+                if (charsRead == -1) {
+                    break;
+                }
+                writer.write (buffer, 0, charsRead);
+            } while (true);
+
+            reader.close();
+        } catch (IOException e) {
+            final Logger logger = new Logger(this);
+            logger.error("LogSending", "Failed creating logcat logs", e);
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    logger.error("LogSending", "Failed closing reader", e);
+                }
+            }
+        }
     }
 }
