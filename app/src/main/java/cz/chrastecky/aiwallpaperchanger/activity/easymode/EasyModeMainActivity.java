a87034dc70bcc17eeee1afa229b745cd39de0d84
Dominik Chrastecky <dominik@chrastecky.cz>
2024-08-04 16:28:32 +0200

    Deserialize prompts

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/easymode/EasyModeMainActivity.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/easymode/EasyModeMainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import cz.chrastecky.aiwallpaperchanger.BuildConfig;
 import cz.chrastecky.aiwallpaperchanger.databinding.ActivityEasyModeMainBinding;
+import cz.chrastecky.aiwallpaperchanger.dto.EasyModePrompt;
+import cz.chrastecky.aiwallpaperchanger.easymode.EasyModePromptManager;
 import cz.chrastecky.aiwallpaperchanger.helper.Logger;
 import cz.chrastecky.aiwallpaperchanger.helper.ThreadHelper;
 
 public class EasyModeMainActivity extends AppCompatActivity {
     private final Logger logger = new Logger(this);
+    private final EasyModePromptManager promptManager = new EasyModePromptManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class EasyModeMainActivity extends AppCompatActivity {
         ActivityEasyModeMainBinding binding = ActivityEasyModeMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+
+        List<EasyModePrompt> prompts = promptManager.getEnrichedPrompts(promptManager.getPrompts()).join();
     }
 
     private void setupExceptionLogging() {
