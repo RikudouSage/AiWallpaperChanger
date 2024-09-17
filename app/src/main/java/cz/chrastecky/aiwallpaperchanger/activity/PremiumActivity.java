b146eebee55461977cd9430929da8a9a1da01a16
Rikudou_Sage <dominik@chrastecky.cz>
2024-09-17 23:25:13 +0200

    Chore: Display correct pricing for Google Play (#107)

--- app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/PremiumActivity.java
+++ app/src/main/java/cz/chrastecky/aiwallpaperchanger/activity/PremiumActivity.java
@@ -7,6 +7,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,6 +30,7 @@ import cz.chrastecky.aiwallpaperchanger.BuildConfig;
 import cz.chrastecky.aiwallpaperchanger.R;
 import cz.chrastecky.aiwallpaperchanger.databinding.ActivityPremiumBinding;
 import cz.chrastecky.aiwallpaperchanger.helper.BillingHelper;
+import cz.chrastecky.aiwallpaperchanger.helper.DateInterval;
 import cz.chrastecky.aiwallpaperchanger.helper.Logger;
 
 public class PremiumActivity extends AppCompatActivity {
@@ -110,15 +112,86 @@ public class PremiumActivity extends AppCompatActivity {
         if (phases.isEmpty()) {
             return;
         }
-        final ProductDetails.PricingPhase phase = phases.get(0);
-        if (phase == null) {
-            return;
+
+        if (phases.size() == 1) {
+            final ProductDetails.PricingPhase phase = phases.get(0);
+            final String price = phase.getFormattedPrice();
+            runOnUiThread(() -> binding.premiumDescription.setText(getString(R.string.app_premium_description_with_price, price)));
+        } else {
+            List<String> prices = new ArrayList<>();
+
+            int i = 0;
+            for (ProductDetails.PricingPhase phase : phases) {
+                final String price;
+                if (phase.getPriceAmountMicros() == 0) {
+                    price = getString(R.string.app_price_free);
+                } else {
+                    price = phase.getFormattedPrice();
+                }
+
+                StringBuilder result = new StringBuilder();
+
+                if (i != 0) {
+                    result.append(getString(R.string.app_price_other_phase_prefix));
+                    result.append(" ");
+                }
+                result.append(price);
+                result.append(" ");
+
+                if (i == 0) {
+                    result.append(getString(R.string.app_price_first_phase));
+                } else if (i != phases.size() - 1){
+                    result.append(getString(R.string.app_price_other_phase));
+                }
+
+                if (i != phases.size() - 1) {
+                    result.append(" ");
+                    result.append(intervalToString(new DateInterval(phase.getBillingPeriod())));
+                }
+                prices.add(result.toString());
+                ++i;
+            }
+
+            runOnUiThread(() -> {
+                binding.premiumDescription.setText(getString(
+                        R.string.app_premium_description_with_price,
+                        "\n\n - " + String.join("\n - ", prices)
+                ));
+            });
+        }
+    }
+
+    private String intervalToString(@NonNull final DateInterval interval) {
+        if (interval.getDays() != 0) {
+            if (interval.getDays() == 1) {
+                return interval.getDays() + " " + getString(R.string.app_period_day_one);
+            } else {
+                return interval.getDays() + " " + getString(R.string.app_period_day_other);
+            }
+        }
+        if (interval.getMonths() != 0) {
+            if (interval.getMonths() == 1) {
+                return interval.getMonths() + " " + getString(R.string.app_period_month_one);
+            } else {
+                return interval.getMonths() + " " + getString(R.string.app_period_month_other);
+            }
+        }
+        if (interval.getYears() != 0) {
+            if (interval.getYears() == 1) {
+                return interval.getYears() + " " + getString(R.string.app_period_year_one);
+            } else {
+                return interval.getYears() + " " + getString(R.string.app_period_year_other);
+            }
+        }
+        if (interval.getWeeks() != 0) {
+            if (interval.getWeeks() == 1) {
+                return interval.getWeeks() + " " + getString(R.string.app_period_week_one);
+            } else {
+                return interval.getWeeks() + " " + getString(R.string.app_period_week_other);
+            }
         }
 
-        final String price = phase.getFormattedPrice();
-        runOnUiThread(() -> {
-            binding.premiumDescription.setText(getString(R.string.app_premium_description_with_price, price));
-        });
+        return "";
     }
 
     private void loadPurchases() {
